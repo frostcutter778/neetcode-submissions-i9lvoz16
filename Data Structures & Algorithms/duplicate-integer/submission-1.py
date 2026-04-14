e0cb3430feeb0f768dc3d5931d6b1f924f96cbe6
Adittya Soukarjya Saha <63427018+frostcutter778@users.noreply.github.com>
2026-04-13 22:09:11 -0400

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasDuplicate(self, nums):
+        nums_set = set(nums) #set only stores unique values, so the new set will differ in length from the original array because it will not store duplicates
+        if len(nums_set) == len(nums): #check the length to see if equal, then no duplicate, else duplicate found and return true
+            return False
+        else:
+            return True
